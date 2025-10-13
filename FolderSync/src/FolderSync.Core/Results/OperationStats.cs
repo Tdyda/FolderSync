486b5ac23e3d174a3f2155e95728c4a0edecd430
Tomasz Dyda <tdyda@tablefoods.com>
2025-10-13 13:31:19 +0200

    feat(report): output clear structured summary at end of each cycle (copied, updated, deleted, dirs created, elapsed time) #12

---
+++ FolderSync/src/FolderSync.Core/Results/OperationStats.cs
@@ -0,0 +1,10 @@
+﻿namespace FolderSync.Core.Results;
+
+public class OperationStats
+{
+    public int FilesCopied { get; set; }
+    public int FilesUpdated { get; set; }
+    public int DirsCreated { get; set; }
+    public int FilesDeleted { get; set; }
+    public int DirsDeleted { get; set; }
+}
